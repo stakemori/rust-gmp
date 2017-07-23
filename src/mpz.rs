0d34747f6f71eb34aab71a24a4d5201b2d4a5801
Sho Takemori <stakemorii@gmail.com>
2017-07-23 21:36:18 +0200

    Implement operators for &mut Mpz

--- src/mpz.rs
+++ src/mpz.rs
@@ -836,6 +836,13 @@ macro_rules! impl_oper {
             }
         }
 
+        impl<'a> $tr<&'a mut Mpz> for Mpz {
+            type Output = Mpz;
+            fn $meth(self, other: &mut Mpz) -> Mpz {
+                (&self).$meth(other)
+            }
+        }
+
         impl<'a> $tr<Mpz> for &'a Mpz {
             type Output = Mpz;
             fn $meth(self, other: Mpz) -> Mpz {
